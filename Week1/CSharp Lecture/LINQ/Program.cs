d4b7c988e44307b47f1370b8c5e644304e59c633
Jegor Laptev <jelapt@ttu.ee>
2019-02-04 19:39:50 +0200

    LINQ Lecture (start)

--- Week1/CSharp Lecture/LINQ/Program.cs
+++ Week1/CSharp Lecture/LINQ/Program.cs
@@ -10,6 +10,71 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            List<Car> myCars = new List<Car>() {
+                new Car() { VIN="A1", Make = "BMW", Model= "550i", StickerPrice=55000, Year=2009},
+                new Car() { VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=35000, Year=2010},
+                new Car() { VIN="C3", Make="BMW", Model = "745li", StickerPrice=75000, Year=2008},
+                new Car() { VIN="D4", Make="Ford", Model="Escape", StickerPrice=25000, Year=2008},
+                new Car() { VIN="E5", Make="BMW", Model="55i", StickerPrice=57000, Year=2010}
+            };
+
+            // LINQ query
+            
+            var bmws = from car in myCars
+                       where car.Make == "BMW"
+                       && car.Year == 2010
+                       select car;
+            
+
+            
+            var orderedCars = from car in myCars
+                              orderby car.Year descending
+                              select car;
+
+
+            // LINQ method
+
+            //1st = 2
+            foreach (var car in bmws)
+            {
+                Console.WriteLine("{0} {1}", car.Model, car.VIN);
+            }
+
+            //2nd
+            var bmws1 = myCars.Where(p => p.Make == "BMW" && p.Year == 2010);
+
+            foreach (var car in bmws1)
+            {
+                Console.WriteLine("{0} {1}", car.Model, car.VIN);
+            }
+
+            //3rd = 4
+
+            foreach (var car in orderedCars)
+            {
+                Console.WriteLine("{0} {1}", car.Year, car.Model, car.VIN);
+            }
+
+
+            //4th
+            var orderedCars2 = myCars.OrderByDescending(p => p.Year);
+
+            foreach (var car in orderedCars2)
+            {
+                Console.WriteLine("{0} {1}", car.Year, car.Model, car.VIN);
+            }
+
+
+
         }
+    }
+
+    class Car
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double StickerPrice { get; set; }
     }
 }
