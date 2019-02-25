00a896fa12ac462b4d53ff965637025f25a212ae
Jegor Laptev <jelapt@ttu.ee>
2019-02-25 16:18:53 +0200

    MVC Learning

--- Week4/MVC/MyFirstMVC/Controllers/TestController.cs
+++ Week4/MVC/MyFirstMVC/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyFirstMVC.Models;
 
 namespace MyFirstMVC.Controllers
 {
@@ -15,6 +16,12 @@ namespace MyFirstMVC.Controllers
 
         public ActionResult GetView()
         {
+            Employee emp = new Employee();
+            emp.FirstName = "Jegor";
+            emp.LastName = "Laptev";
+            emp.Solary = 5000;
+
+            ViewData["Employee"] = emp;
             return View("MyView");
         }
     }
---
+++ Week4/MVC/MyFirstMVC/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstMVC.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Solary { get; set; }
+    }
+}
