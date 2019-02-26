38d404e64ac848a84ca05dd9e05fc9fb0d34602c
Jegor Laptev <jelapt@ttu.ee>
2019-02-26 19:42:14 +0200

    Get started with EF Core in an ASP.NET MVC web app (Continue)

--- Week5/MVCwithEFCore/ContosoUniversity/Startup.cs
+++ Week5/MVCwithEFCore/ContosoUniversity/Startup.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoUniversity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace ContosoUniversity
 {
@@ -30,6 +33,11 @@ namespace ContosoUniversity
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<SchoolContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc();
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
