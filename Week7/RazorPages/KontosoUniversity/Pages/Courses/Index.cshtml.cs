6a17add44915ed0ac0ca77caa9cc61193cadaaaf
Jegor Laptev <jelapt@ttu.ee>
2019-03-13 18:17:28 +0200

    Read Related Data - Lecture (Save point)

---
+++ Week7/RazorPages/KontosoUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KontosoUniversity.Models;
+
+namespace KontosoUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KontosoUniversity.Models.SchoolContext _context;
+
+        public IndexModel(KontosoUniversity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
---
+++ Week7/RazorPages/KontosoUniversity/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KontosoUniversity.Models;
+using KontosoUniversity.Models.SchoolViewModels;  // Add VM
+
+namespace KontosoUniversity.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KontosoUniversity.Models.SchoolContext _context;
+
+        public IndexModel(KontosoUniversity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+        public InstructorIndexData Instructor { get; set; }
+        public int InstructorID { get; set; }
+
+        public async Task OnGetAsync(int? id)
+        {
+            Instructor = new InstructorIndexData();
+            Instructor.Instructors = await _context.Instructors
+                  .Include(i => i.OfficeAssignment)
+                  .Include(i => i.CourseAssignments)
+                    .ThenInclude(i => i.Course)
+                  .AsNoTracking()
+                  .OrderBy(i => i.LastName)
+                  .ToListAsync();
+
+            if (id != null)
+            {
+                InstructorID = id.Value;
+            }
+        }
+    }
+}
