d65293e249a87a43f5dc39eb3a2e64e802d3465a
Jegor Laptev <jelapt@ttu.ee>
2019-03-13 16:27:05 +0200

    RazorPages Started + Broken project added

--- Week1/CSharp Lecture/Collections/Program.cs
+++ Week1/CSharp Lecture/Collections/Program.cs
@@ -94,4 +94,4 @@ namespace Collections
 
     }
 }
-}
+
---
+++ Week7/RazorPages/KontosoUniversity/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KontosoUniversity.Models;
+
+namespace KontosoUniversity.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KontosoUniversity.Models.SchoolContext _context;
+
+        public IndexModel(KontosoUniversity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}
